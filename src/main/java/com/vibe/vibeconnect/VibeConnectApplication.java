59dd3753b37e2b8fa9010918b373e22f656626d4
Devaki Nanda Gopal <devakinandagopal@Devakis-MacBook-Air.local>
2025-09-21 16:14:27 +0530

    Initial setup

---
+++ src/main/java/com/vibe/vibeconnect/VibeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.vibe.vibeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibe/vibeconnect/VibeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibe.vibeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
